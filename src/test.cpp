0df434f2176e8faf3ade78d35f9f62d9dbf926d7
Ian Diaz <idiaz@itba.edu.ar>
2018-09-06 14:48:13 -0300

    nothing relevant

--- src/test.cpp
+++ src/test.cpp
@@ -3,9 +3,9 @@
 #include "./dummys/dummys.h"
 
 int main (void){
-    std::list<terrains_d> hola1;
-    std::list<units_d>  hola2;
-    std::list<buildings_d> hola3;
+    std::vector<terrains_d> hola1;
+    std::vector<units_d>  hola2;
+    std::vector<buildings_d> hola3;
     Graphics chau(hola1,hola2,hola3);
     return 0;
 }
